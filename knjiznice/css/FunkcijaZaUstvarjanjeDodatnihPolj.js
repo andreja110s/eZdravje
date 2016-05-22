c1f64c024fbb02597392dc0a4f0ed3223e93644b
Andreja Jerič <aj6009@student.uni-lj.si>
2016-05-22 21:00:05 +0000

    Sm misnla kej nrdit ma sm dejansko nrdila skor neč

--- knjiznice/css/FunkcijaZaUstvarjanjeDodatnihPolj.js
+++ knjiznice/css/FunkcijaZaUstvarjanjeDodatnihPolj.js
@@ -2,26 +2,7 @@ var seznamHrane= document.getElementById("hrana");
 
 function dodajPolje() {
     if (tisto!=0) {
-        var vrni= "<select id='hrana'>
-						<option value='0'></option>
-						<option value='1'>Hamburger</option>
-						<option value='2'>Krompirček</option>
-						<option value='3'>Sadje</option>
-						<option value='4'>Testenine/lazanja</option>
-						<option value='5'>Pohano meso</option>
-						<option value='6'>Goveja juha</option>
-						<option value='7'>Zelenjavna juha</option>
-						<option value='8'>Gobova juha</option>
-						<option value='9'>Čips</option>
-						<option value='10'>Pica</option>
-						<option value='11'>Rižota</option>
-						<option value='12'>Pečene piščančje perutničke</option>
-						<option value='13'>Piščančji file</option>
-						<option value='14'>Sladoled</option>
-						<option value='15'>Torta</option>
-						<option value='16'>Pire krompir</option>
-						<option value='17'>Zelenjava</option>
-						</select>"
+        var vrni= ""
     }
     return vrni;
 }
